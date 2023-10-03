96fe12c472c611cc259e3f70d322ac4cd7243307
Chisom Daniel <chinwezechisom@gmail.com>
2023-10-03 09:22:11 +0100

    easy peasy

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -34,14 +34,3 @@ def canUnlockAll(boxes):
             return True
 
     return False
-
-
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
