0728e168bc93b0dafea16f810cbd13557db7c251
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-29 09:36:08 +0100

    perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Calculate the perimeter of an island'''
+from typing import List
+
+
+def island_perimeter(grid: List[List[int]]):
+    '''calculate the perimeter of the island'''
+    start = False
+    count = 0
+
+    if grid == [] or grid == [[]]:
+        return 0
+
+    length = len(grid[0])
+    # vertical scan
+    for i in range(length):
+        for each in grid:
+            if (each[i] == 1) and (start == False):
+                count += 1
+                start = True
+            if (each[i] == 0) and (start == True):
+                count += 1
+                start = False
+    
+    # horizontal scan
+    start = False
+    for each in grid:
+        for j in each:
+            if (j == 1) and (start == False):
+                count += 1
+                start = True
+            if (j == 0) and (start == True):
+                count += 1
+                start = False
+
+    return count
