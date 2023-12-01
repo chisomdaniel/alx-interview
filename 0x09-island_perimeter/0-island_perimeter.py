53e5779e6cabe389837c1c94ef066479a935d4cf
Chisom Daniel <chinwezechisom@gmail.com>
2023-12-01 13:32:15 +0100

    corrected

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,22 +13,26 @@ def island_perimeter(grid):
     length = len(grid[0])
     # vertical scan
     for i in range(length):
+        idx = 0
         for each in grid:
+            idx += 1
             if (each[i] == 1) and (start is False):
                 count += 1
                 start = True
-            if (each[i] == 0) and (start is True):
+            if ((each[i] == 0) or (idx == len(grid))) and (start is True):
                 count += 1
                 start = False
 
     # horizontal scan
     start = False
     for each in grid:
+        idx = 0
         for j in each:
+            idx += 1
             if (j == 1) and (start is False):
                 count += 1
                 start = True
-            if (j == 0) and (start is True):
+            if ((j == 0) or (idx == length)) and (start is True):
                 count += 1
                 start = False
 
