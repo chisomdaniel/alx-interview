5478f411b5bf409ae43d5a3c7314ef44d50d71c0
Chisom Daniel <chinwezechisom@gmail.com>
2023-12-07 20:54:19 +0100

    try

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -59,9 +59,3 @@ def isWinner(x, nums):
         return 'Maria'
     elif final_winner[-1] == 'Maria':
         return 'Ben'
-
-
-
-print("Winner: {}".format(isWinner(3, [4, 5, 1])))
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
-
